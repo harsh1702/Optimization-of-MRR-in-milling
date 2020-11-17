059e1b443992d277d66d7f37fa84915f5c836e39
Harsh Kulkarni <harsh1702@gmail.com>
2020-11-17 13:32:03 +0530

    Added the symbolic function

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 %% Here we have to declare whether we are maximizing or minimizing the function, not needed for the final project
-type = input('enter whether you would maximize or minimize');
-n = input('enter the number of constraints');
+%uncooment the variable input parameters later
+type = 'min';
+n = 2;
+%type = input('enter whether you would maximize or minimize');
+%n = input('enter the number of variables');
 
 %% Decalring the tolerance
 
@@ -8,15 +11,18 @@ tol = 10^-3;    %consider reducing the value of tol
 
 %% first we do an unconstrained optimization for the initial point
 
-x0 = zeros(1:n);
-[q, f] = penfunc(x0, 0);    %need to check once if this is valid
+numPar = 2;
+x = sym('x', [1 numPar]);
+
+X0 = zeros(1:n);
+[q, f] = sympenfunc(X0, 0);    %need to check once if this is valid
 if (type == 'max')
     invpenfunc = 1/q;
-    val = fminsearch(invpenfunc, x0);
-    max = penfunc(x, 0);
+    f_max = matlabFunction(invpenfunc, 'Vars', {x});
+    [X, max, exitflag] = fminsearch(f_max, X0);
 else
-    val = fminsearch(q, x0);
-    max = penfunc(x, 0);
+    f_min = matlabFunction(q, 'Vars', {x});
+    [X, max, exitflag] = fminsearch(f_min, X0);
 end
 %update x0 with the value at which unconstrained optimization occurs
     
@@ -26,9 +32,10 @@ c = 10000;  %some large initial value
 alpha = 10; %set the multiplier value
 
 while (err > tol)
-    [q, f] = penfunc(x,c);
-    [G1, G2, G3] = gradient(q);
-    g = [G1; G2; G3];
+    [q, f] = sympenfunc(X,c);
+    g = gradient(q);
+    %[G1, G2, G3] = gradient(q);
+    %g = [G1; G2; G3];
 
     J = jacobian([G1, G2, G3], [x(1), x(2), x(3)]); %using the Jacobian matrix method to solve the system of non-linear equations
 
